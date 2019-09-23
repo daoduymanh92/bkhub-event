eb907aaa825753631c44a502b305bbb645dc1085
daoduymanhcs <manhdao@x3english.com>
2019-09-23 11:45:39 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,21 @@ Route::get('/', function () {
 });
 
 Route::post('post-register-email', 'CustomerController@postRegisterEmail');
-Auth::routes();
+// Auth::routes();
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::middleware(['auth'])->group(function () {
 	Route::get('test', 'CustomerController@test');
-
 	Route::get('book-list', 'CustomerController@bookingList');
 });
